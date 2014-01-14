5ac18d86678e99d9b426debd9d1d215337032086
dually8 <exerarol@gmail.com>
2014-01-14 01:44:00 -0500

    1-14-2013

--- Tutorial01/Assets/Scripts/CoinPickup.cs
+++ Tutorial01/Assets/Scripts/CoinPickup.cs
@@ -7,11 +7,12 @@ public class CoinPickup : MonoBehaviour {
 	
 	void OnTriggerEnter (Collider info) 
 	{
-		if (info.tag == "Player") 
+		//if (info.tag == "Player") 
+		if(info.collider.name == "Player")
 		{
 			Instantiate(coinEffect, transform.position, transform.rotation);
 			//Destroy(effect, 3);
-			Destroy (gameObject); 
+			Destroy (this.gameObject); 
 			// add coin counter here
 			GameMaster.currentScore++;
 			Debug.Log(GameMaster.currentScore);
